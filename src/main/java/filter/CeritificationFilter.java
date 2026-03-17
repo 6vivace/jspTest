6865c3f78fb511cc3bcdb73c72ebd135f2ca18bc
6vivace <6vivace@gmaiil.com>
2026-03-17 14:16:45 +0900

    20260317_1416 JSP파일업로드

--- src/main/java/filter/CeritificationFilter.java
+++ src/main/java/filter/CeritificationFilter.java
@@ -12,37 +12,37 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/login/*")	//미완성_260304
+@WebFilter("/*")
 public class CeritificationFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpSession session = req.getSession();	// req.getSession(false) : 세션이 없으면 만들지 않음.
 		
 		String login = session.getAttribute("sLogin")==null ? "" : (String) session.getAttribute("sLogin");
 		
-//		System.out.println("login : " + login);
+		//System.out.println("login : " + login);
 		
 		String uri = req.getRequestURI();
 		
 		
-		if(!uri.contains("/Main") && !uri.contains("/guest/*") && !uri.contains("/login/*") && !uri.contains("/study/*")) {
-			System.out.println("인증창 통과중...");
-			if(!uri.contains("/css/") && !uri.contains("/js/")) {
+		if(!uri.contains("/") && !uri.contains("/Main") && !uri.contains("/guest/") && !uri.contains("/login/")) {
+			System.out.println("인증창 통과중....");
+			if(!uri.contains("/css/") && !uri.contains("/js/") && !uri.contains("/images/")) {
 				request.setCharacterEncoding("utf-8");
 				response.setContentType("text/html; charset=utf-8");
 			}
 			if(login == null || login.equals("")) {
 				PrintWriter out = response.getWriter();
-				System.out.println("로그인 실패통과...");
+				System.out.println("로그인 실패통과....");
 				out.println("<script>");
 				out.println("alert('로그인후 사용하세요.');");
 				out.println("location.href='"+req.getContextPath()+"/login/Login?loginSw=main_Login'");
 				out.println("</script>");
 			}
 		}
+		
 		
 		chain.doFilter(request, response);
 		
--- src/main/webapp/WEB-INF/study2/fileUpload/fileUploadForm.jsp
+++ src/main/webapp/WEB-INF/study2/fileUpload/fileUploadForm.jsp
@@ -4,9 +4,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>fileUploadForm.jsp</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>fileUploadForm.jsp</title>
   <%@ include file="/include/bs5.jsp" %>
 </head>
 <body>
@@ -14,16 +14,19 @@
 <jsp:include page="/include/nav.jsp" />
 <p><br/></p>
 <div class="container">
-	<h2>fileUpload 연습폼</h2>
-	<hr/>
-	<h4>COS라이브러리를 이용한 파일 업로드</h4>
-	<div>(https://servlets.com/cos/)</div>
-	<hr/>
-	<div>
-		<a href="FileUpload1.st" class="btn btn-success">파일업로드연습1</a>
-	</div>
-	<hr/>
-	<div><a href="FileDownLoad.st" class="btn btn-primary">다운로드폼으로 이동하기</a></div>
+  <h2>fileUpload 연습폼</h2>
+  <hr/>
+  <h4>COS라이브러리를 이용한 파일 업로드</h4>
+  <div>(https://servlets.com/cos/)</div>
+  <hr/>
+  <div>
+    <a href="FileUpload1.st" class="btn btn-success">싱글파일업로드</a>
+    <a href="FileUpload2.st" class="btn btn-info">멀티파일업로드1</a>
+    <a href="FileUpload3.st" class="btn btn-warning">멀티파일업로드2</a>
+    <a href="FileUpload4.st" class="btn btn-secondary">멀티파일업로드3</a>
+  </div>
+  <hr/>
+  <div><a href="FileDownLoad.st" class="btn btn-primary">다운로드폼으로 이동하기</a></div>
 </div>
 <p><br/></p>
 <jsp:include page="/include/footer.jsp" />
